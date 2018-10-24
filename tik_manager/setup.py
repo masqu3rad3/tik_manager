c1efc512ddc75243982d01a88ded6c196a7e6bfa
Arda Kutlu <ardakutlu@gmail.com>
2018-10-25 00:18:46 +0300

    easy setup WIP

---
+++ tik_manager/setup.py
@@ -0,0 +1,16 @@
+try:
+    import hedehot
+except ImportError:
+    print "he"
+
+
+# Maya Setup
+# ----------
+
+# Find shelves under ~/Documents/maya/<Maya Version> for each Maya Version
+# Build content for "shelf_SceneManager.mel"
+# Create or overwrite the file
+
+# Find userSetup.py under ~/Documents/maya/scripts
+    # If there is, edit it
+    # Else create a new one
