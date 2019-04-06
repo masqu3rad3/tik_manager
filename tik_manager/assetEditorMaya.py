39482c8eed690bfd540c4bc76097527138933620
Arda Kutlu <ardakutlu@gmail.com>
2019-04-06 14:18:11 +0300

    assetLibrary wip / seperated into standalone (assetLibrary.py) and Editor (assetEditorMaya etc.) modules

---
+++ tik_manager/assetEditorMaya.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------------------------------
+# Copyright (c) 2017-2018, Arda Kutlu (ardakutlu@gmail.com)
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#  - Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+#  - Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+#  - Neither the name of the software nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# -----------------------------------------------------------------------------
+
+# Asset save functions for Maya to be used with assetLibrary
+import os
+import pymel.core as pm
+import json
+import logging
+import assetLibrary
+reload(assetLibrary)
+from assetLibrary import AssetLibrary
+from assetLibrary import MainUI as baseUI
+
+
+
+
+__author__ = "Arda Kutlu"
+__copyright__ = "Copyright 2018, Asset Library"
+__credits__ = []
+__license__ = "GPL"
+__maintainer__ = "Arda Kutlu"
+__email__ = "ardakutlu@gmail.com"
+__status__ = "Development"
+
+logging.basicConfig()
+logger = logging.getLogger('AssetLibrary')
+logger.setLevel(logging.WARNING)
+
+
+class AssetEditor(assetLibrary.AssetLibrary):
+    def __init__(self):
+        super(AssetEditor, self).__init__()
+
+    def saveAsset(self, assetName, screenshot=True, moveCenter=False, selectionOnly=True, exportUV=True, exportOBJ=True, **info):
+        """
+        Saves the selected object(s) as an asset into the predefined library
+        Args:
+            assetName: (Unicode) Asset will be saved as with this name
+            screenshot: (Bool) If true, screenshots (Screenshot, Thumbnail, Wireframe, UV Snapshots) will be taken with playblast. Default True
+            directory: (Unicode) Default Library location. Default is predefined outside of this class
+            moveCenter: (Bool) If True, selected object(s) will be moved to World 0 point. Pivot will be the center of selection. Default False
+            **info: (Any) Extra information which will be hold in the .json file
+
+        Returns:
+            None
+
+        """
+        logger.info("Saving the asset")
+        originalPath = pm.sceneName()
+
+
+        assetDirectory = os.path.join(self.directory, assetName)
+
+        ## TODO // in a scenario where a group object selected, select all meshes under the group recursively (you did that before somewhere else)
+        if selectionOnly:
+            selection = pm.ls(sl=True, type="transform")
+            if len(selection) == 0:
+                pm.warning("No object selected, nothing to do")
+                return
+        else:
+            selection = pm.ls(type="transform")
+
+
+        if not os.path.exists(assetDirectory):
+            os.mkdir(assetDirectory)
+
+        possibleFileHolders = pm.listRelatives(selection, ad=True, type=["mesh", "nurbsSurface"])
+
+        allFileTextures = []
+        for obj in possibleFileHolders:
+            fileNodes = self.findFileNodes(obj)
+            fileTextures = self.filePass(fileNodes, assetDirectory)
+            allFileTextures += fileTextures
+
+        allFileTextures = self.makeUnique(allFileTextures)
+
+        if moveCenter:
+            pm.select(selection)
+            slGrp = pm.group(name=assetName)
+
+            pm.xform(slGrp, cp=True)
+
+            tempLoc = pm.spaceLocator()
+            tempPo = pm.pointConstraint(tempLoc, slGrp)
+            pm.delete(tempPo)
+            pm.delete(tempLoc)
+
+        thumbPath, ssPath, swPath = self.previewSaver(assetName, assetDirectory, selectionOnly=selectionOnly, uvSnap=exportUV)
+
+
+        if selectionOnly:
+            pm.select(selection)
+            # objName = "N/A"
+            if exportOBJ:
+                if not pm.pluginInfo('objExport', l=True, q=True):
+                    pm.loadPlugin('objExport')
+                objName = pm.exportSelected(os.path.join(assetDirectory, assetName), type="OBJexport", force=True,
+                                            options="groups=1;ptgroups=1;materials=1;smoothing=1;normals=1", pr=True,
+                                            es=True)
+            else:
+                objName = "NA"
+            maName = pm.exportSelected(os.path.join(assetDirectory, assetName), type="mayaAscii")
+
+            # selection for poly evaluate
+            pm.select(possibleFileHolders)
+            polyCount = pm.polyEvaluate(f=True)
+            tiangleCount = pm.polyEvaluate(t=True)
+
+        else:
+            pm.select(d=True)
+            # objName = "N/A"
+            if exportOBJ:
+                objName = pm.exportAll(os.path.join(assetDirectory, assetName), type="OBJexport", force=True,
+                                    options="groups=1;ptgroups=1;materials=1;smoothing=1;normals=1", pr=True)
+            else:
+                objName = "NA"
+            maName = pm.exportAll(os.path.join(assetDirectory, assetName), type="mayaAscii")
+
+            # selection for poly evaluate
+            pm.select(d=True)
+            polyCount = pm.polyEvaluate(f=True)
+            tiangleCount = pm.polyEvaluate(t=True)
+
+        ## Json stuff
+
+        info['assetName'] = assetName
+        info['objPath'] = self.pathOps(objName, "basename")
+        info['maPath'] = self.pathOps(maName, "basename")
+        info['thumbPath'] = self.pathOps(thumbPath, "basename")
+        info['ssPath'] = self.pathOps(ssPath, "basename")
+        info['swPath'] = self.pathOps(swPath, "basename")
+        info['textureFiles'] = allFileTextures
+        info['Faces/Trianges'] = ("%s/%s" % (str(polyCount), str(tiangleCount)))
+        info['sourceProject'] = originalPath
+
+        # query the number of faces
+        # pm.polyEvaluate(f=True)
+        # Result: 16
+
+        # query the number of triangles
+        # pm.polyEvaluate(t=True)
+
+        propFile = os.path.join(assetDirectory, "%s.json" % assetName)
+
+        with open(propFile, "w") as f:
+            json.dump(info, f, indent=4)
+
+        self[assetName] = info
+
+        ## TODO // REVERT BACK
+        # if not originalPath == "":
+        #     pm.openFile(originalPath, force=True)
+        #     os.remove(newScenePath)
+
+class MainUI(baseUI):
+    def __init__(self, callback=None):
+        super(MainUI, self).__init__()
+
+        # self.manager = MayaManager()
+        # problem, msg = self.manager._checkRequirements()
+        # if problem:
+        #     self.close()
+        #     self.deleteLater()
+        #
+        # self.callbackIDList=[]
+        # if self.isCallback:
+        #     self.callbackIDList = self.manager._createCallbacks(self.isCallback, self.windowName)
+        #
+        # self.buildUI()
+        # self.initMainUI(newborn=True)
+        # self.extraMenus()
+
+
--- tik_manager/assetLibrary.py
+++ tik_manager/assetLibrary.py
@@ -37,6 +37,8 @@ import sys
 import shutil
 import _version
 
+import time
+
 FORCE_QT4 = bool(os.getenv("FORCE_QT4"))
 
 # Enabele FORCE_QT4 for compiling with pyinstaller
@@ -132,7 +134,28 @@ def getMainWindow():
     else:
         return None
 
-class AssetLibrary(dict):
+def _loadJson(file):
+    """Loads the given json file"""
+    try:
+        with open(file, 'r') as f:
+            data = json.load(f)
+            return data
+    except ValueError:
+        # msg = "Corrupted JSON file => %s" % file
+        # logger.error(msg)
+        # self._exception(200, msg)
+        return(-1) # code for corrupted json file
+
+def _dumpJson(data, file):
+    """Saves the data to the json file"""
+    name, ext = os.path.splitext(file)
+    tempFile = "{0}.tmp".format(name)
+    with open(tempFile, "w") as f:
+        json.dump(data, f, indent=4)
+    shutil.copyfile(tempFile, file)
+    os.remove(tempFile)
+
+class AssetLibrary(object):
     """
     Asset Library Logical operations Class. This Class holds the main functions (save,import,scan)
     """
@@ -194,7 +217,6 @@ class AssetLibrary(dict):
         """
         if not os.path.exists(self.directory):
             return
-        self.clear()
         # first collect all the json files from second level subfolders
         subDirs = next(os.walk(self.directory))[1]
         # for dir in subDirs:
@@ -207,9 +229,13 @@ class AssetLibrary(dict):
         thumbPath = os.path.join(self.directory, assetName, "%s_thumb.jpg" %assetName)
         return thumbPath
 
+    def showInExplorer(self, assetName):
+        path = os.path.join(self.directory, assetName)
+        os.startfile(path)
+
     def getData(self, assetName):
         jsonFile = os.path.join(self.directory, assetName, "%s.json" %assetName)
-        data = self._loadJson(jsonFile)
+        data = _loadJson(jsonFile)
         return data
 
     def importAsset(self, name, copyTextures, mode="maPath"):
@@ -246,28 +272,6 @@ class AssetLibrary(dict):
         pass
 
 
-    def _loadJson(self, file):
-        """Loads the given json file"""
-        try:
-            with open(file, 'r') as f:
-                data = json.load(f)
-                return data
-        except ValueError:
-            msg = "Corrupted JSON file => %s" % file
-            # logger.error(msg)
-            self._exception(200, msg)
-            # return -2 # code for corrupted json file
-
-
-    def _dumpJson(self, data, file):
-        """Saves the data to the json file"""
-        name, ext = os.path.splitext(file)
-        tempFile = "{0}.tmp".format(name)
-        with open(tempFile, "w") as f:
-            json.dump(data, f, indent=4)
-        shutil.copyfile(tempFile, file)
-        os.remove(tempFile)
-
     def _exception(self, code, msg):
         """Exception report function. Throws a log error and raises an exception"""
         logger.error("Exception %s" %self.errorCodeDict[code])
@@ -289,6 +293,10 @@ class MainUI(QtWidgets.QMainWindow):
         # Set Stylesheet
         dirname = os.path.dirname(os.path.abspath(__file__))
         stylesheetFile = os.path.join(dirname, "CSS", "darkorange.stylesheet")
+
+        self.homedir = os.path.expanduser("~")
+        self.DocumentsDir = "Documents" if BoilerDict["Environment"] == "Standalone" else ""
+        self.settingsFile = os.path.join(self.homedir, self.DocumentsDir, "assetLibraryConfig.json")
 
         self.setObjectName(BoilerDict["Environment"])
         self.resize(670, 624)
@@ -343,11 +351,26 @@ class MainUI(QtWidgets.QMainWindow):
 
         self.masterLayout.addWidget(self.tabWidget)
 
-        testPath="E:\_Sel"
-        preTab = libraryTab(testPath)
-        self.tabWidget.addTab(preTab, "TEST")
-        preTab.setLayout(preTab.layout)
 
+
+        for lib in self.getLibraryPaths():
+            name = lib[0]
+            path = lib[1]
+            if not os.path.exists(path):
+                logger.warning("Cannot reach library path: \n%s \n Removing from the database..." % (path))
+                self.removeLibrary(lib)
+                continue
+            preTab = libraryTab(path)
+            self.tabWidget.addTab(preTab, name)
+            preTab.setObjectName(name)
+            preTab.setLayout(preTab.layout)
+
+        self.tabWidget.currentChanged.connect(self.refreshTab)
+        # self.tabWidget.currentChanged.connect(self.tabWidget.currentWidget().populate)
+        self.refreshTab()
+
+    def refreshTab(self):
+        self.tabWidget.currentWidget().populate()
 
         #
         # libs = self.settings(mode="load")
@@ -392,6 +415,28 @@ class MainUI(QtWidgets.QMainWindow):
         # removeTabAction.triggered.connect(self.deleteCurrentTab)
         #
         # self.currentChanged.connect(self.createNewTab)  # changed!
+
+    def getLibraryPaths(self):
+        try:
+            libraryPaths = _loadJson(self.settingsFile)
+        except IOError: # it file does not exist
+            libraryPaths = []
+            _dumpJson(libraryPaths, self.settingsFile)
+        return libraryPaths
+
+    def addLibrary(self, path, name):
+        libraryPaths = self.getLibraryPaths()
+        newItem = [name, path]
+        libraryPaths.append(newItem)
+        _dumpJson(libraryPaths, self.settingsFile)
+        pass
+
+    def removeLibrary(self, item):
+        print item
+        libraryPaths = self.getLibraryPaths()
+        libraryPaths.pop(libraryPaths.index(item))
+        _dumpJson(libraryPaths, self.settingsFile)
+        pass
 
     def on_context_menu(self, point):
         # show context menu
@@ -438,6 +483,7 @@ class MainUI(QtWidgets.QMainWindow):
             self.setCurrentIndex(currentIndex - 1)
             self.removeTab(currentIndex)
             self.settings(mode="remove", itemIndex=currentIndex)
+
 
     def settings(self, mode, name=None, path=None, itemIndex=None, item=None):
         """
@@ -544,7 +590,7 @@ class MainUI(QtWidgets.QMainWindow):
         logger.warning("Settings file not changed")
 
 class libraryTab(QtWidgets.QWidget):
-    viewModeState = 1
+    viewModeState = -1
 
     def __init__(self, directory):
         self.directory = directory
@@ -568,7 +614,7 @@ class libraryTab(QtWidgets.QWidget):
         # self.exportOBJ_def = True
         # self.selectionOnly_def = True
 
-        # self.library = assetLibrary(directory)
+        self.library = AssetLibrary(directory)
         self.buildTabUI()
 
     def buildTabUI(self):
@@ -605,12 +651,12 @@ class libraryTab(QtWidgets.QWidget):
         self.horizontalLayout.setContentsMargins(-1, 6, -1, -1)
         self.horizontalLayout.setSpacing(6)
 
-        self.filter_label_2 = QtWidgets.QLabel(self.frame)
+        self.filter_label = QtWidgets.QLabel(self.frame)
 
-        self.horizontalLayout.addWidget(self.filter_label_2)
+        self.horizontalLayout.addWidget(self.filter_label)
 
-        self.filter_lineEdit_2 = QtWidgets.QLineEdit(self.frame)
-        self.horizontalLayout.addWidget(self.filter_lineEdit_2)
+        self.filter_lineEdit = QtWidgets.QLineEdit(self.frame)
+        self.horizontalLayout.addWidget(self.filter_lineEdit)
 
         self.rightBelow_verticalLayout.addLayout(self.horizontalLayout)
 
@@ -686,6 +732,144 @@ class libraryTab(QtWidgets.QWidget):
         self.rightUp_gridLayout.addWidget(self.importAndCopy_pushButton, 0, 0, 1, 1)
 
         self.gridLayout_2.addLayout(self.rightUp_gridLayout, 0, 0, 1, 1)
+
+        self.filter_lineEdit.textChanged.connect(self.populate)
+
+        # RIGHT CLICK MENU
+        # ----------------
+
+        self.assets_listWidget.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        self.assets_listWidget.customContextMenuRequested.connect(self.onContextMenu_assets)
+        self.popMenu_assets = QtWidgets.QMenu()
+
+        self.assets_rcItem_0 = QtWidgets.QAction('View as Icons', self)
+        self.popMenu_assets.addAction(self.assets_rcItem_0)
+        self.assets_rcItem_0.triggered.connect(lambda: self.rcAction_assets("viewModeChange"))
+
+        self.assets_rcItem_1 = QtWidgets.QAction('Show in Explorer', self)
+        self.popMenu_assets.addAction(self.assets_rcItem_1)
+        self.assets_rcItem_1.triggered.connect(lambda: self.rcAction_assets("showInExplorer"))
+
+
+
+
+
+        # self.populate()
+
+    def populate(self):
+        """
+        UI populate function - linkes to the assetLibrary.scan()
+        Returns:
+
+        """
+        filterWord = self.filter_lineEdit.text()
+
+        self.assets_listWidget.clear()
+        self.library.scanAssets()
+
+
+        if self.viewModeState == 1:
+            self.assets_listWidget.setViewMode(QtWidgets.QListWidget.IconMode)
+            self.assets_listWidget.setIconSize(QtCore.QSize(100, 100))
+            self.assets_listWidget.setMovement(QtWidgets.QListView.Static)
+            self.assets_listWidget.setResizeMode(QtWidgets.QListWidget.Adjust)
+            self.assets_listWidget.setGridSize(QtCore.QSize(100*1.2, 100*1.4))
+
+            # self.assets_listWidget.addItems(self.filterList(self.library.assetsList, filterWord))
+            filteredItems = self.filterList(self.library.assetsList, filterWord)
+            for itemName in filteredItems:
+                item = QtWidgets.QListWidgetItem(itemName)
+                thumbPath = self.library.getThumbnail(itemName)
+
+                icon = QtGui.QIcon(thumbPath)
+                item.setIcon(icon)
+
+                self.assets_listWidget.addItem(item)
+
+        else:
+            self.assets_listWidget.setViewMode(QtWidgets.QListWidget.ListMode)
+            self.assets_listWidget.setGridSize(QtCore.QSize(15,15))
+            self.assets_listWidget.addItems(self.filterList(self.library.assetsList, filterWord))
+
+
+        # for name, info in sorted(self.library.items()):
+        #
+        #     # if there is a filterword, filter the item
+        #     if filterWord != "" and filterWord.lower() not in name.lower():
+        #         continue
+        #     # We create an item for the list widget and tell it to have our controller name as a label
+        #     item = QtWidgets.QListWidgetItem(name)
+        #
+        #     # We set its tooltip to be the info from the json
+        #     # The pprint.pformat will format our dictionary nicely
+        #     item.setToolTip(pprint.pformat(info))
+        #
+        #     # Finally we check if there's a screenshot available
+        #     thumb = info.get('thumbPath')
+        #     asset = info.get('assetName')
+        #     thumbPath = os.path.join(self.directory, asset, thumb)
+        #     # If there is, then we will load it
+        #     if thumb:
+        #         # So first we make an icon with the path to our screenshot
+        #         icon = QtGui.QIcon(thumbPath)
+        #         # then we set the icon onto our item
+        #         item.setIcon(icon)
+        #
+        #     # Finally we add our item to the list
+        #     self.listWidget.addItem(item)
+
+    def filterList(self, sourceList, filterWord):
+        if filterWord == "":
+            return sourceList
+        else:
+            filteredList = [item for item in sourceList if (filterWord.lower() in item.lower())]
+            return filteredList
+
+    def onContextMenu_assets(self, point):
+        # This method IS Software Specific
+        row = self.assets_listWidget.currentRow()
+        if row == -1:
+            return
+        #
+        #     self.scenes_rcItem_1.setEnabled(False)
+        #     self.scenes_rcItem_2.setEnabled(False)
+        #     self.scenes_rcItem_3.setEnabled(False)
+        #     self.scenes_rcItem_4.setEnabled(False)
+        #     self.scenes_rcItem_5.setEnabled(False)
+        #     self.scenes_rcItem_6.setEnabled(True)
+        # else:
+        #     self.scenes_rcItem_1.setEnabled(os.path.isdir(manager.currentBaseScenePath))
+        #     self.scenes_rcItem_2.setEnabled(os.path.isdir(manager.currentPreviewPath))
+        #     self.scenes_rcItem_3.setEnabled(True)
+        #     self.scenes_rcItem_4.setEnabled(True)
+        #     # show context menu
+        #     self.scenes_rcItem_5.setEnabled(os.path.isdir(os.path.join(manager.projectDir, "images", manager.currentBaseSceneName)))
+        #     self.scenes_rcItem_6.setEnabled(True)
+
+        self.popMenu_assets.exec_(self.assets_listWidget.mapToGlobal(point))
+
+
+    def rcAction_assets(self, item):
+        currentItem = self.assets_listWidget.currentItem()
+        name = currentItem.text()
+        info = self.library.getData(name)
+
+        if item == 'showInExplorer':
+            self.library.showInExplorer(name)
+
+        elif item == 'viewModeChange':
+            self.viewModeState = self.viewModeState * -1
+            if self.viewModeState == 1:
+                self.assets_rcItem_0.setText("View As List")
+                self.populate()
+                # self.assets_listWidget.setViewMode(QtWidgets.QListWidget.IconMode)
+            elif self.viewModeState == -1:
+                self.assets_rcItem_0.setText("View As Icons")
+                self.populate()
+                # self.assets_listWidget.setViewMode(QtWidgets.QListWidget.ListMode)
+
+
+
 
 if __name__ == '__main__':
     os.environ["FORCE_QT4"] = "True"
